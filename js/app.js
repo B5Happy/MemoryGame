dd8d401cacdef88d20ab6c7b1ad9038fbfb36142
Saly <saly@juke4me.com>
2018-05-24 15:06:14 +0200

    clear come bug

--- js/app.js
+++ js/app.js
@@ -1,76 +1,35 @@
 /*
- * Create a list that holds all of your cards
+ * Create a list that holds all cards
  */
 
-cards = ["anchor","anchor","diamond","diamond","paper-plane-o","paper-plane-o","bolt","bolt","cube","cube","leaf","leaf","bicycle","bicycle","bomb","bomb"];
-tcards = ["fa fa-anchor","fa fa-anchor","fa fa-diamond","fa fa-diamond","fa fa-paper-plane-o","fa fa-paper-plane-o",
-            "fa fa-bolt","fa fa-bolt","fa fa-cube","fa fa-cube","fa fa-leaf","fa fa-leaf","fa fa-bicycle","fa fa-bicycle","fa fa-bomb","fa fa-bomb"];
 tscards = ["fa-anchor","fa-anchor","fa-diamond","fa-diamond","fa-paper-plane-o","fa-paper-plane-o",
             "fa-bolt","fa-bolt","fa-cube","fa-cube","fa-leaf","fa-leaf","fa-bicycle","fa-bicycle","fa-bomb","fa-bomb"];
 
 
-
-
 /*
  * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
  */
 
-//newCards = shuffle(tcards);
-//const newLi = document.createElement('li');
-//const ul = document.createElement('ul');
-//newLi.classList.add('card');
+document.getElementById("box").addEventListener("click", click);
 
-/*newCards.forEach(function(card) {
-
-    const newI = document.createElement('i');
-    newI.classList.add("card");
-    
-
-});*/
-
-/*for(let i = 0 ; i < newCards.length; i++) {
-
-    const newI = document.createElement('i');
-    newI.classList.add(newCards[i]);
-    newLi.appendChild(newI);
-    ul.appendChild(newLi);
-
-}*/
-
-document.getElementById("ddd").addEventListener("click", click);
-
-// test
 function myFunction() {
 
-    //newCards = shuffle(tscards);
-   // const newLi = document.createElement('li');
-    //const ul = document.createElement('ul');
-    //const ul = document.getElementById("ddd");
-    //ul.classList.add('deck');
-    //newLi.classList.add('card');
-    const ul = document.getElementById("ddd");
-
-    const div = document.getElementsById("test");
-
-   const newI = document.createElement('i');
-
-   tscards.forEach(function(card) {
-
-    const newLi = document.createElement('li');
-   
-    newLi.classList.add('card');
+    newCards = shuffle(tscards);
+    const ul = document.getElementById("box");
     const newI = document.createElement('i');
+
+    tscards.forEach(function(card) {
+
+        const newLi = document.createElement('li');
+        newLi.classList.add('card');
+        const newI = document.createElement('i');
         newI.classList.add('fa',card);
         newLi.appendChild(newI);
-    ul.appendChild(newLi);
-    //div.appendChild(ul);
-   // alert(ul);
+        ul.appendChild(newLi);
+  
     });
 
-   //div.appendChild(ul);
+
     
 } 
 
@@ -91,14 +50,7 @@ function shuffle(array) {
 
 
 /*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
+ * set up the event listener for a card.
  */
 
 
@@ -112,19 +64,17 @@ function click() {
 
     show(event);
     addToList(event);
-
+    
     if (list.length === 2){
 
         if (list[0].innerHTML === list[1].innerHTML){
 
-            //alert("match")
             match();
+
         } else {
 
-            //setTimeout(dontMatch, 50000);
             dMatch();
             setTimeout(dontMatch, 2500);
-            //alert("work ?")
         }
         
     }
@@ -142,21 +92,16 @@ function click() {
 
 function show(event) {
 
-   // event.preventDefault();
-
-    console.log(event.target)
     event.target.classList.add('open','show');
-    //event.target.disabled = true;
-
+   
 }
 
 function addToList(event){
 
-   ev = event.target ;
+    ev = event.target;
     list.push(ev);
-    console.log(list);
-    
 
+    
 }
 
 function match(){
@@ -168,23 +113,19 @@ function match(){
     list =[];
     matchList++;
 
-
 }
 
 function dMatch() {
 
     list[0].classList.remove("open");
-    list[1].classList.remove("open");
+    list[1].classList.remove("open"); 
     list[0].classList.add('dmatch');
     list[1].classList.add('dmatch');
-    
     
 }
  
 function  dontMatch() {
 
-
-   // sleep(5000);
     list[0].classList.remove("dmatch", "show");
     list[1].classList.remove("dmatch", "show");
     list =[];
@@ -197,7 +138,7 @@ function increase(){
     a++;
     document.getElementsByTagName("span")[0].innerHTML = a;
     
-   // alert(clickCount);
+  
 } 
 
 function reset() {
@@ -206,32 +147,27 @@ function reset() {
 
   function star() {
 
-    //alert (document.getElementsByClassName("stars")[0].innerHTML)
+    
    const star = document.getElementById("star1");
    const star2 = document.getElementById("star2");
    const star3 = document.getElementById("star3");
-  // alert ( document.getElementById("star1").innerHTML);
+ 
 
     if (  document.getElementsByTagName("span")[0].innerHTML > 28 ) {
 
 
-        //document.getElementsByClassName("stars")[0].removeChild(document.getElementById("star1").innerHTML); 
         star3.remove();
     
     }  else {
 
     if ( document.getElementsByTagName("span")[0].innerHTML > 24 )  {
 
-
-        //document.getElementsByClassName("stars")[0].removeChild(document.getElementById("star1").innerHTML); 
         star2.remove();
     
     }  else {
 
     if ( document.getElementsByTagName("span")[0].innerHTML > 20 )  {
 
-
-        //document.getElementsByClassName("stars")[0].removeChild(document.getElementById("star1").innerHTML); 
         star.remove();
     
             }  
