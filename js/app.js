b9663d9077f2cec143dc340e95b4dc17468910e7
Saly <saly@juke4me.com>
2018-06-10 02:47:52 +0200

    Add timer and winning page

--- js/app.js
+++ js/app.js
@@ -2,40 +2,55 @@
  * Create a list that holds all cards
  */
 
-tscards = ["fa-anchor","fa-anchor","fa-diamond","fa-diamond","fa-paper-plane-o","fa-paper-plane-o",
+cardsListe = ["fa-anchor","fa-anchor","fa-diamond","fa-diamond","fa-paper-plane-o","fa-paper-plane-o",
             "fa-bolt","fa-bolt","fa-cube","fa-cube","fa-leaf","fa-leaf","fa-bicycle","fa-bicycle","fa-bomb","fa-bomb"];
 
 
 /*
- * Display the cards on the page
- */
+* Creation of event listener to trigger a click function when clicked
+*/
 
 document.getElementById("box").addEventListener("click", click);
+let sec = 0;
+
+/*
+ * Display the cards on the page and setting the timer
+ */
 
 function myFunction() {
 
-    newCards = shuffle(tscards);
-    const ul = document.getElementById("box");
-    const newI = document.createElement('i');
+    newCards = cardsListe; //shuffle(cardsListe);
+    const displayAllCard = document.getElementById("box");
+    const newCard = document.createElement('i');
 
-    tscards.forEach(function(card) {
+    cardsListe.forEach(function(card) {
 
-        const newLi = document.createElement('li');
-        newLi.classList.add('card');
-        const newI = document.createElement('i');
-        newI.classList.add('fa',card);
-        newLi.appendChild(newI);
-        ul.appendChild(newLi);
+        const cardHolder = document.createElement('li');
+        cardHolder.classList.add('card');
+        const newCard = document.createElement('i');
+        newCard.classList.add('fa',card);
+        cardHolder.appendChild(newCard);
+        displayAllCard.appendChild(cardHolder);
   
     });
 
 
+    clock = setInterval("watchStart()", 1000);
+
     
 } 
 
+/*
+*Creation of a timer
+*/
+function watchStart() {
+	sec++;
+	document.getElementById("timer").innerHTML = sec;
+}
+
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
+    let currentIndex = array.length, temporaryValue, randomIndex;
 
     while (currentIndex !== 0) {
         randomIndex = Math.floor(Math.random() * currentIndex);
@@ -56,11 +71,20 @@ function shuffle(array) {
 
 list =[];
 let matchList = 0;
-let a = 0;
- 
+let moveCount = 0;
+let cardCliked = false ; 
+let numStar = "3 Stars"
+
+/*
+*
+*/
 
 
 function click() {
+
+    cardCliks(event);
+
+   if ( cardCliked === true){
 
     show(event);
     addToList(event);
@@ -71,9 +95,10 @@ function click() {
 
             match();
 
-            if ( matchList === 8 ){
+            if ( matchList === 1 ){
 
-                alert("You win ! with "+ document.getElementsByTagName("span")[0].innerHTML + " moves" )
+                setTimeout(win, 1500);
+                //alert("You win ! with "+ document.getElementsByTagName("span")[0].innerHTML + " moves" )
             
             }
 
@@ -85,15 +110,38 @@ function click() {
         
     }
 
-    increase();
+    increase(event);
     star();
 
     
-
+    }
 
 }
 
+/*
+* Verify if the card is clicked only once
+*/
 
+function cardCliks(event){
+
+    console.log(event.target.classList);
+
+    const contentClicked = event.target.classList.contains("deck"); 
+    const contentClicked2 = event.target.classList.contains("open"); 
+
+    if ( contentClicked === true || contentClicked2 === true){
+
+        cardCliked = false;
+      
+    } else {
+
+        cardCliked = true;
+    }
+}
+
+/*
+* Display the card clicked
+*/
 function show(event) {
 
     event.target.classList.add('open','show');
@@ -112,6 +160,9 @@ function addToList(event){
     
 }
 
+/*
+* Verify if the cards match
+*/
 function match(){
 
     list[0].classList.remove("open", "show");
@@ -122,7 +173,9 @@ function match(){
     matchList++;
 
 }
-
+/*
+* Event when the card dosent match
+*/
 function dMatch() {
 
     list[0].classList.remove("open");
@@ -140,19 +193,26 @@ function  dontMatch() {
 
 
 }
-
-function increase(){
+/*
+* Set up of a move counter
+*/
+function increase(event){
    
-    a++;
-    document.getElementsByTagName("span")[0].innerHTML = a;
+    ev = event.target;
+    moveCount++;
+    document.getElementsByTagName("span")[0].innerHTML = moveCount;
     
   
 } 
-
+/*
+* Reset the game
+*/
 function reset() {
     location.reload();
   }
-
+/*
+* Ratings for the game based on number of moves used to complete the game
+*/
   function star() {
 
     
@@ -161,25 +221,57 @@ function reset() {
    const star3 = document.getElementById("star3");
  
 
-    if (  document.getElementsByTagName("span")[0].innerHTML > 28 ) {
+    /*if (  document.getElementsByTagName("span")[0].innerHTML > 28 ) {
 
 
         star3.remove();
     
-    }  else {
+    }  else { */
 
     if ( document.getElementsByTagName("span")[0].innerHTML > 24 )  {
 
+        numStar = "2 Stars"
         star2.remove();
     
     }  else {
 
     if ( document.getElementsByTagName("span")[0].innerHTML > 20 )  {
 
+
+        numStar = "1 Star"
         star.remove();
     
             }  
         }
-    }
+    //}
+
+}
+/*
+* Results (Details of the win) 
+*/
+function win () {
+    
+    //alert("You win ! with "+ document.getElementsByTagName("span")[0].innerHTML + " moves" )
+    clearInterval(clock);
+    //document.getElementsByTagName("BODY")[0].style.backgroundColor = "white";
+    document.body.innerHTML = "";
+    winingAnonce = document.createElement("div");
+    winingTitle = document.createElement("h2");
+    wininingText = document.createTextNode("Congratulations! You Won!");
+    winingTitle.appendChild(wininingText);
+    results = document.createElement("p");
+    resultsText = document.createTextNode(` In ${sec} seconds with ${moveCount} Moves and ${numStar}.`);
+    results.appendChild(resultsText);
+    button = document.createElement("button");
+    buttonText = document.createTextNode("Play again!");
+    button.appendChild(buttonText);
+    button.addEventListener("click", reset);
+    button.classList.add("btn");
+    button.classList.add("btn-outline-primary");
+    winingAnonce.classList.add("container");
+    winingAnonce.appendChild(winingTitle);    
+    winingAnonce.appendChild(results);
+    winingAnonce.appendChild(button);                                 
+    document.body.appendChild(winingAnonce);        
 
 }
